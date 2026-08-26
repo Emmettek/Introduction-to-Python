fff5f8c0c957aa9b2b67e1d8afb38dde05734513
Emmettek <oleczka2603@gmail.com>
2026-08-26 18:34:16 +0200

    conditional statements

---
+++ module2/ex02/password.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+user_password = input("Enter password: ") 
+
+password = "Python is awesome"
+
+if user_password == password: 
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
